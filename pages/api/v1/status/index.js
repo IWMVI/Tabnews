4cf52cbe2bb8fdb9a487687695d3665c72cc416d
Wallace Martins <wmvwallace@gmail.com>
2025-03-18 02:02:14 -0300

    feat: atualiza endpoint de status para retornar a data de atualização

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,8 @@
-import database from "infra/database.js";
-
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Valor" });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
